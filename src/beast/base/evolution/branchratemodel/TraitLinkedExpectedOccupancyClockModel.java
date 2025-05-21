5af29f32056deb23dfc5b2b4d0561913458f7c50
jessiejofielding <51216246+jessiejofielding@users.noreply.github.com>
2025-05-21 13:09:24 -0400

    clean up

--- src/beast/base/evolution/branchratemodel/TraitLinkedExpectedOccupancyClockModel.java
+++ src/beast/base/evolution/branchratemodel/TraitLinkedExpectedOccupancyClockModel.java
@@ -1,9 +1,7 @@
 package beast.base.evolution.branchratemodel;
 
 import beast.base.core.Description;
-import beast.base.core.Function;
 import beast.base.core.Input;
-import beast.base.evolution.substitutionmodel.SVSGeneralSubstitutionModelNew;
 import beast.base.evolution.tree.Node;
 
 /**
@@ -13,53 +11,10 @@ import beast.base.evolution.tree.Node;
 @Description("Defines a mean rate for each branch in the beast.tree.")
 public class TraitLinkedExpectedOccupancyClockModel extends AbstractTraitLinkedBranchRateModel {
 
-    public Input<Function> geoClockRateInput = new Input<>("traitClockRate", "the clock rate for the Ancestral Reconstruction Tree Likelihood");
-
-    public Input<SVSGeneralSubstitutionModelNew> svsInput = new Input<SVSGeneralSubstitutionModelNew>("substitutionModel", "testing the substitution model input");
-
-    Function traitClockRate;
-    SVSGeneralSubstitutionModelNew svs;
-
-    double[][] qMatrix;
-
     @Override
-    public void initAndValidate() {
-        super.initAndValidate();
-        traitClockRate = geoClockRateInput.get();
-        svs = svsInput.get();
-
-//        TODO(jf): confirm q matrix is actually being updated here? would prefer not to get it every single branch but
-        qMatrix = svs.getRateMatrix();
+    public boolean isExpectedOccupancy() {
+        return true;
     }
-
-    private double[] getOccupancy(final int parentTrait, final int currentTrait, final Double time) {
-        // TODO(jf): check all this math, and that we're always setting occupancy[0] to the occupancy in trait 0
-        double alpha = qMatrix[0][1];
-        double beta = qMatrix[1][0];
-        double k = alpha + beta;
-        double expkt = Math.exp(-k * time);
-        double[] occupancy = new double[2];
-
-        if (parentTrait == 0 && currentTrait == 0) {
-            occupancy[0] = (1 / k) * (
-                    (Math.pow(beta, 2) * time + 2 * alpha * beta / k * (1 - expkt) + Math.pow(alpha, 2) * time * expkt) /
-                            (beta + alpha * expkt)
-            )/time;
-        } else if ((parentTrait == 0 && currentTrait == 1) || (parentTrait == 1 && currentTrait == 0)) {
-            occupancy[0] = (1 / k) * (
-                    (beta * time - alpha * time * expkt + (alpha - beta) / k * (1 - expkt)) /
-                            (1 - expkt)
-            )/time;
-        } else if (parentTrait == 1 && currentTrait == 1) {
-            occupancy[0] = (1 / k) * (
-                    (alpha * beta * time - 2 * alpha * beta / k * (1 - expkt) + alpha * beta * time * expkt) /
-                            (alpha + beta * expkt)
-            )/time;
-        }
-        occupancy[1] = 1-occupancy[0];
-        return occupancy;
-    }
-
 
     @Override
     public double getRateForBranch(final Node node) {
