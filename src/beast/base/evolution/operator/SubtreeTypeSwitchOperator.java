3607ebb21f62bf362006d79836ceb25b07974c4b
jessiejofielding <51216246+jessiejofielding@users.noreply.github.com>
2025-07-02 13:41:45 -0400

    Add subtree type switch operator (#4)
    * add subtree type switch operator
    
    * Update version.xml

---
+++ src/beast/base/evolution/operator/SubtreeTypeSwitchOperator.java
@@ -0,0 +1,115 @@
+package beast.base.evolution.operator;
+
+import beast.base.core.Input;
+import beast.base.evolution.alignment.Alignment;
+import beast.base.evolution.tree.Node;
+import beast.base.evolution.tree.Tree;
+import beast.base.inference.parameter.IntegerParameter;
+import beast.base.inference.parameter.Parameter;
+import beast.base.inference.util.InputUtil;
+import beast.base.util.Randomizer;
+
+import java.util.Arrays;
+
+public class SubtreeTypeSwitchOperator extends TreeOperator {
+    final public Input<IntegerParameter> traitsInput = new Input<>("trait", "a real or integer parameter to sample individual values for", Input.Validate.REQUIRED, Parameter.class);
+    final public Input<Alignment> dataInput = new Input<>("data", "trait data for the tips", Input.Validate.OPTIONAL);
+
+    IntegerParameter traits;
+    int lowerInt, upperInt;
+
+    boolean[] isAmbiguous;
+
+
+    // empty constructor to facilitate construction by XML + initAndValidate
+    public SubtreeTypeSwitchOperator() {
+    }
+
+    public SubtreeTypeSwitchOperator(Tree tree) {
+        try {
+            initByName(treeInput.getName(), tree);
+        } catch (Exception e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            throw new RuntimeException("Failed to construct Trait Operator.");
+        }
+    }
+
+    @Override
+    public void initAndValidate() {
+        traits = traitsInput.get();
+
+        lowerInt = traits.getLower();
+        upperInt = traits.getUpper();
+
+        isAmbiguous = new boolean[treeInput.get().getNodeCount()];
+        Arrays.fill(isAmbiguous, true);
+
+        Alignment data = dataInput.get();
+        for (Node node : treeInput.get().getExternalNodes()) {
+            String taxon = node.getID();
+            int nodeNum = node.getNr();
+            if (data == null) {
+                isAmbiguous[nodeNum] = false;
+            }
+            else {
+                int taxonIndex = data.getTaxonIndex(taxon);
+                if (taxonIndex == -1) {
+                    if (taxon.startsWith("'") || taxon.startsWith("\"")) {
+                        taxonIndex = data.getTaxonIndex(taxon.substring(1, taxon.length() - 1));
+                    }
+                    if (taxonIndex == -1) {
+                        throw new RuntimeException("Could not find sequence " + taxon + " in the alignment");
+                    }
+                }
+                // this only handles data with one pattern
+                isAmbiguous[nodeNum] = data.getDataType().isAmbiguousCode(data.getPattern(taxonIndex, 0));
+            }
+        }
+
+    }
+
+    /**
+     * change the parameter and return the hastings ratio.
+     *
+     * @return log of Hastings Ratio, or Double.NEGATIVE_INFINITY if proposal should not be accepted *
+     */
+    @Override
+    public double proposal() {
+        final Tree tree = (Tree) InputUtil.get(treeInput, this);
+
+        // randomly select internal node
+        final int nodeCount = tree.getNodeCount();
+
+        // Abort if no non-root internal nodes
+        if (tree.getInternalNodeCount() == 1)
+            return Double.NEGATIVE_INFINITY;
+
+        Node node;
+        do {
+            final int nodeNr = nodeCount / 2 + 1 + Randomizer.nextInt(nodeCount / 2);
+            node = tree.getNode(nodeNr);
+        } while (node.isLeaf()); // subtree operator shouldn't pick a subtree that starts at the leaves
+        int newValue = Randomizer.nextInt(upperInt - lowerInt + 1) + lowerInt; // from 0 to n-1, n must > 0,
+        setSubtree(node, newValue);
+
+        if (markCladesInput.get()) {
+            node.makeAllDirty(Tree.IS_DIRTY);
+        }
+
+        return 0.0;
+    }
+
+    private void setSubtree(Node node, int newValue) {
+        int nodeNum = node.getNr();
+        if (node.isLeaf()) {
+            if (isAmbiguous[nodeNum]) {
+                traits.setValue(nodeNum, newValue);
+            }
+            return;
+        }
+        traits.setValue(nodeNum, newValue);
+        for (Node childNode : node.getChildren()) {
+            setSubtree(childNode, newValue);
+        }
+    }
+}
