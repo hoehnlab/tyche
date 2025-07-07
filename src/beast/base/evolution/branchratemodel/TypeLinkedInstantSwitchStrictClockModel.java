081287cc0dca5ea94289a9d53f9aea601ea94ba4
jessiejofielding <51216246+jessiejofielding@users.noreply.github.com>
2025-07-07 16:33:48 -0400

    rename instant switch clock model class

---
+++ src/beast/base/evolution/branchratemodel/TypeLinkedInstantSwitchStrictClockModel.java
@@ -0,0 +1,23 @@
+package beast.base.evolution.branchratemodel;
+
+import beast.base.core.Description;
+import beast.base.evolution.tree.Node;
+
+/**
+ * @author Jessie Fielding
+ */
+
+@Description("Defines a type-linked rate for each branch in the beast.tree, where the branch is assumed to be entirely in the child state.")
+public class TypeLinkedInstantSwitchStrictClockModel extends AbstractTypeLinkedClockModel {
+
+    public double getBranchRate(final Node node) {
+
+        if (node.isRoot()) {
+            return 1.0;
+        }
+//        get this node's type
+        int type = (int) nodeTypes.getArrayValue(node.getNr());
+        return getTypeLinkedRate(type);
+
+    }
+}
