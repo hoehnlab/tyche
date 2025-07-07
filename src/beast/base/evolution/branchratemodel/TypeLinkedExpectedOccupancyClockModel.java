75112e80172d21df2c9f637dd1d374fa5d9b3d46
jessiejofielding <51216246+jessiejofielding@users.noreply.github.com>
2025-07-07 16:33:25 -0400

    rename expected occupancy clock model class

---
+++ src/beast/base/evolution/branchratemodel/TypeLinkedExpectedOccupancyClockModel.java
@@ -0,0 +1,32 @@
+package beast.base.evolution.branchratemodel;
+
+import beast.base.core.Description;
+import beast.base.evolution.tree.Node;
+
+/**
+ * @author Jessie Fielding
+ */
+
+@Description("Defines a type-linked rate for each branch in the beast.tree, where the branch rate is calculated from the expected occupancy in each type.")
+public class TypeLinkedExpectedOccupancyClockModel extends AbstractTypeLinkedClockModel {
+
+    @Override
+    public boolean isExpectedOccupancy() {
+        return true;
+    }
+
+    @Override
+    public double getBranchRate(final Node node) {
+
+        if (node.isRoot()) {
+            return 1.0;
+        }
+
+        int type = (int) nodeTypes.getArrayValue(node.getNr());
+        int parentType = (int) nodeTypes.getArrayValue(node.getParent().getNr());
+        double typeTime = node.getLength() * typeSwitchClockRate.getArrayValue();
+        double[] occupancy = getOccupancy(parentType, type, typeTime, node.getNr());
+        return (getTypeLinkedRate(0)*occupancy[0] + getTypeLinkedRate(1)*occupancy[1]);
+    }
+
+}
