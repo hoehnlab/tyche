5e0ed7c0659752856386c2121895623d0064c1c5
jessiejofielding <51216246+jessiejofielding@users.noreply.github.com>
2025-08-19 23:40:11 -0400

    add mixed switch model (#10)

---
+++ src/tyche/evolution/branchratemodel/TycheMixedSwitchClockModel.java
@@ -0,0 +1,30 @@
+package tyche.evolution.branchratemodel;
+
+import beast.base.core.Description;
+import beast.base.evolution.tree.Node;
+
+@Description("Defines a type-linked rate for each branch in the beast.tree, where the branch rate is calculated from the expected occupancy in each type on branches with differently typed nodes, and on branches with same-typed nodes is assumed to be entirely in that state.")
+public class TycheMixedSwitchClockModel extends AbstractTycheTypeLinkedClockModel {
+    @Override
+    public boolean isExpectedOccupancy() {
+        return true;
+    }
+
+    @Override
+    public double getBranchRate(final Node node) {
+
+        if (node.isRoot()) {
+            return 1.0;
+        }
+
+        int type = (int) nodeTypes.getArrayValue(node.getNr());
+        int parentType = (int) nodeTypes.getArrayValue(node.getParent().getNr());
+        if (type == parentType) {
+            return getTypeLinkedRate(type);
+        }
+        double typeTime = node.getLength() * typeSwitchClockRate.getArrayValue();
+        double[] occupancy = getOccupancy(parentType, type, typeTime, node.getNr());
+        return (getTypeLinkedRate(0)*occupancy[0] + getTypeLinkedRate(1)*occupancy[1]);
+    }
+
+}
